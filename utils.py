e572b9d614945101f38d1f1c83140dfa78c9338e
dajuno <dajuno@riseup.net>
2016-05-13 20:21:20 -0300

    add on_cluster() check

--- utils.py
+++ utils.py
@@ -2,6 +2,14 @@
     for functions of general use and utility'''
 
 
+def on_cluster():
+    import platform
+    import re
+    uname = platform.uname()
+    pattern = re.compile("^(leftraru\d)|(cn\d\d\d)")
+    return bool(pattern.match(uname[1]))
+
+
 def trymkdir(path):
     ''' mkdir if path does not exist yet '''
     import os
